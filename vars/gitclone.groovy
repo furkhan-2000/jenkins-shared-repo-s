65f4a715d49e04bbae72ae811338cd3fd381bde0
SERVER <mehdifurkhan00@gmail.com>
2025-04-21 01:17:20 +0530

    Delete vars directory

--- vars/gitclone.groovy
+++
@@ -1,5 +0,0 @@
-def call(String url, String branch) {
-    echo "its code block"
-    git url: "${url}", branch: "${branch}"
-    echo "successfully git repo got cloned"
-}
--- vars/snippets.groovy
+++
@@ -1,7 +0,0 @@
-def call(){
-  sh "mkdir deleteplease"
-  sh "rmdir deleteplease"
-  sh "mkdir oberoie"
-  echo "please delete oberoie"
-  sh "rm -rf oberoie"
-}
