22e48471e5e1672eba253732fda7f87710d9b142
SERVER <mehdifurkhan00@gmail.com>
2025-04-22 21:11:49 +0530

    Update docker_login.groovy

--- vars/docker_login.groovy
+++ vars/docker_login.groovy
@@ -1,14 +1,14 @@
-def call(String project, String imageTag, String dockerhubuser) {
-   withCredentials([usernamePassword(
-                    credentialsId: 'dockerHubCred',
-                    usernameVariable: 'DOCKERHUB_USERNAME',
-                    passwordVariable: 'DOCKERHUB_PASSWORD'
-                )]) {
-                    echo "This is tagging part"
-                    sh "docker tag testing-web:latest ${DOCKERHUB_USERNAME}/shark:workouts"
-                    sh "docker login -u ${DOCKERHUB_USERNAME} -p ${DOCKERHUB_PASSWORD}"
-                    echo "successfully logged in ${DOCKERHUB_USERNAME}"
-                    sh "docker push ${DOCKERHUB_USERNAME}/shark:workouts"
-                    echo "successfully image pushed"
-                }
+def call(String project, String imageTag) {
+    withCredentials([usernamePassword(
+        credentialsId: 'dockerHubCred',
+        usernameVariable: 'DOCKERHUB_USERNAME',
+        passwordVariable: 'DOCKERHUB_PASSWORD'
+    )]) {
+        echo "This is tagging part"
+        sh "docker tag ${project}:${imageTag} ${DOCKERHUB_USERNAME}/${project}:${imageTag}"
+        sh "docker login -u ${DOCKERHUB_USERNAME} -p ${DOCKERHUB_PASSWORD}"
+        echo "Successfully logged in as ${DOCKERHUB_USERNAME}"
+        sh "docker push ${DOCKERHUB_USERNAME}/${project}:${imageTag}"
+        echo "Successfully pushed image"
+    }
 }
