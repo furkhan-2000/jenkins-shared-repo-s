57a082f48a2d3bd85dc2634841cc034f75f8b7bb
SERVER <mehdifurkhan00@gmail.com>
2025-04-22 19:27:28 +0530

    Update myecho.groovy

--- vars/myecho.groovy
+++ vars/myecho.groovy
@@ -1,3 +1,3 @@
 def call() {
-    echo "this is sample of my snippts, i know am still learning"
+    echo "this is sample of my snippts, i know am still learning, this is not that too hard you pepple can also learn"
 }
