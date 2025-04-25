9ed3c652a6524ee66103cb71fd296d65536f04db
SERVER <mehdifurkhan00@gmail.com>
2025-04-26 04:07:18 +0530

    Update docker_login.groovy

--- vars/docker_login.groovy
+++ vars/docker_login.groovy
@@ -1,18 +1,13 @@
 def call(String project,
          String imageTag,
          String credentialsId = 'dockerHubCred') {
-
     withCredentials([usernamePassword(
-        credentialsId: credentialsId,
+        credentialsId: dockerhubCred,
         usernameVariable: 'DOCKERHUB_USERNAME',
         passwordVariable: 'DOCKERHUB_PASSWORD'
     )]) {
         sh "docker tag ${project}:${imageTag} ${DOCKERHUB_USERNAME}/${project}:${imageTag}"
-
         sh "docker login -u ${DOCKERHUB_USERNAME} -p ${DOCKERHUB_PASSWORD}"
-
         sh "docker push ${DOCKERHUB_USERNAME}/${project}:${imageTag}"
-
-        echo "Docker image push complete"
     }
 }
